196334e113fc360a6783bb9fd7ce325774c05873
deep-learning-dynamo <deep-learning-dynamo@gmail.com>
2023-07-06 13:30:39 +0200

    added hello world example

---
+++ other-examples/src/main/java/HelloWorldExample.java
@@ -0,0 +1,19 @@
+import dev.langchain4j.data.message.AiMessage;
+import dev.langchain4j.model.openai.OpenAiChatModel;
+
+public class HelloWorldExample {
+
+    public static void main(String[] args) {
+
+        // Import your OpenAI/HuggingFace API key
+        String apiKey = System.getenv("OPENAI_API_KEY");
+
+        // Create an instance of a model
+        OpenAiChatModel model = OpenAiChatModel.withApiKey(apiKey);
+
+        // Start interacting!
+        AiMessage answer = model.sendUserMessage("Hello world!").get();
+
+        System.out.println(answer.text()); // Hello! How can I assist you today?
+    }
+}
