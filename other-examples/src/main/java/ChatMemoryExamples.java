d2930e6320081b514841ef56c928c1f05d4ba623
deep-learning-dynamo <deep-learning-dynamo@gmail.com>
2023-07-13 14:46:29 +0200

    updated ChatMemoryExamples

--- other-examples/src/main/java/ChatMemoryExamples.java
+++ other-examples/src/main/java/ChatMemoryExamples.java
@@ -1,6 +1,5 @@
 import dev.langchain4j.chain.ConversationalChain;
 import dev.langchain4j.data.message.AiMessage;
-import dev.langchain4j.data.message.UserMessage;
 import dev.langchain4j.memory.ChatMemory;
 import dev.langchain4j.memory.chat.MessageWindowChatMemory;
 import dev.langchain4j.memory.chat.TokenWindowChatMemory;
@@ -9,9 +8,7 @@ import dev.langchain4j.model.openai.OpenAiChatModel;
 import dev.langchain4j.model.openai.OpenAiTokenizer;
 import dev.langchain4j.service.AiServices;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.time.Duration;
 
 import static dev.langchain4j.data.message.UserMessage.userMessage;
@@ -47,29 +44,17 @@ public class ChatMemoryExamples {
 
         public static void main(String[] args) throws IOException {
 
+            String apiKey = System.getenv("OPENAI_API_KEY"); // https://platform.openai.com/account/api-keys
+
             ConversationalChain chain = ConversationalChain.builder()
-                    .chatLanguageModel(OpenAiChatModel.builder()
-                            .modelName(GPT_3_5_TURBO)
-                            .apiKey(System.getenv("OPENAI_API_KEY")) // https://platform.openai.com/account/api-keys
-                            .build())
-                    .chatMemory(MessageWindowChatMemory.builder()
-                            .systemMessage("You are a helpful assistant.")
-                            .capacityInMessages(7)
-                            .build())
+                    .chatLanguageModel(OpenAiChatModel.withApiKey(apiKey))
                     .build();
 
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            while (true) {
-                System.out.print("User: ");
-                String messageFromUser = br.readLine();
+            String answer = chain.execute("Hello, my name is Klaus");
+            System.out.println(answer); // Hello Klaus! How can I assist you today?
 
-                if ("exit".equalsIgnoreCase(messageFromUser)) {
-                    return;
-                }
-
-                String responseFromAi = chain.execute(messageFromUser);
-                System.out.println("AI: " + responseFromAi);
-            }
+            String answerWithName = chain.execute("What is my name?");
+            System.out.println(answerWithName); // Your name is Klaus.
         }
     }
 
@@ -92,19 +77,20 @@ public class ChatMemoryExamples {
                     .tokenizer(new OpenAiTokenizer(GPT_3_5_TURBO))
                     .build();
 
-            UserMessage userMessage = userMessage("Tell me a joke");
-
             // You have full control over the chat memory.
             // You can decide if you want to add a particular message to the memory
             // (e.g. you might not want to store few-shot examples to save on tokens).
             // You can process/modify the message before saving if required.
-            chatMemory.add(userMessage);
 
-            AiMessage aiMessage = model.sendMessages(chatMemory.messages()).get();
+            chatMemory.add(userMessage("Hello, my name is Klaus"));
+            AiMessage answer = model.sendMessages(chatMemory.messages()).get();
+            System.out.println(answer.text()); // Hello Klaus! How can I assist you today?
+            chatMemory.add(answer);
 
-            chatMemory.add(aiMessage);
-
-            System.out.println(aiMessage.text());
+            chatMemory.add(userMessage("What is my name?"));
+            AiMessage answerWithName = model.sendMessages(chatMemory.messages()).get();
+            System.out.println(answerWithName.text()); // Hello Klaus! How can I assist you today?
+            chatMemory.add(answerWithName); // Your name is Klaus.
         }
     }
 }
