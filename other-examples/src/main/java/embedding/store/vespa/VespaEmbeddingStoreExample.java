d21a863473ff0b589d113bb1f2ab4e6052d20c7f
Titov, Alexey <alexey.titov@adesso.de>
2023-09-08 08:30:56 +0200

    Retrofit support

--- other-examples/src/main/java/embedding/store/vespa/VespaEmbeddingStoreExample.java
+++ other-examples/src/main/java/embedding/store/vespa/VespaEmbeddingStoreExample.java
@@ -22,48 +22,66 @@ public class VespaEmbeddingStoreExample {
     EmbeddingStore<TextSegment> embeddingStore = VespaEmbeddingStore
       .builder()
       // server url, e.g. https://alexey-heezer.langchain4j.mytenant346.aws-us-east-1c.dev.z.vespa-app.cloud
-      .url("url")
+      //      .url("url")
+      .url(
+        "https://alexey-heezer.langchain4j.mytenant346.aws-us-east-1c.dev.z.vespa-app.cloud"
+      )
       // local path to the SSL private key file,
       // e.g. /Users/user/.vespa/mytenant346.langchain4j.alexey-heezer/data-plane-private-key.pem
-      .keyPath("keyPath")
+      //      .keyPath("keyPath")
+      .keyPath(
+        "/Users/alexey.titov/.vespa/alexey-heezer.langchain4j.mytenant346/data-plane-private-key.pem"
+      )
       // local path to the SSL certificate file,
       // e.g. /Users/user/.vespa/mytenant346.langchain4j.alexey-heezer/data-plane-public-cert.pem
-      .certPath("certPath")
+      //      .certPath("certPath")
+      .certPath(
+        "/Users/alexey.titov/.vespa/alexey-heezer.langchain4j.mytenant346/data-plane-public-cert.pem"
+      )
       .build();
 
-    InProcessEmbeddingModel embeddingModel = new InProcessEmbeddingModel(ALL_MINILM_L6_V2);
-
-    TextSegment segment1 = TextSegment.from("I like football.");
-    Embedding embedding1 = embeddingModel.embed(segment1);
-    embeddingStore.add(embedding1, segment1);
-
-    TextSegment segment2 = TextSegment.from("I've never been to New York.");
-    Embedding embedding2 = embeddingModel.embed(segment2);
-    embeddingStore.add(embedding2, segment2);
-
-    TextSegment segment3 = TextSegment.from(
-      "But actually we tried our new swimming pool yesterday and it was awesome!"
-    );
-    Embedding embedding3 = embeddingModel.embed(segment3);
-    embeddingStore.add(embedding3, segment3);
-
-    List<String> ids = embeddingStore.addAll(
-      asList(embedding1, embedding2, embedding3),
-      asList(segment1, segment2, segment3)
+    InProcessEmbeddingModel embeddingModel = new InProcessEmbeddingModel(
+      ALL_MINILM_L6_V2
     );
 
-    System.out.println("added/updated records count: " + ids.size()); // 3
+    //    TextSegment segment1 = TextSegment.from("I like football.");
+    //    Embedding embedding1 = embeddingModel.embed(segment1);
+    //    embeddingStore.add(embedding1, segment1);
+    //
+    //    TextSegment segment2 = TextSegment.from("I've never been to New York.");
+    //    Embedding embedding2 = embeddingModel.embed(segment2);
+    //    embeddingStore.add(embedding2, segment2);
+    //
+    //    TextSegment segment3 = TextSegment.from(
+    //      "But actually we tried our new swimming pool yesterday and it was awesome!"
+    //    );
+    //    Embedding embedding3 = embeddingModel.embed(segment3);
+    //    embeddingStore.add(embedding3, segment3);
+    //
+    //    List<String> ids = embeddingStore.addAll(
+    //      asList(embedding1, embedding2, embedding3),
+    //      asList(segment1, segment2, segment3)
+    //    );
+    //
+    //    System.out.println("added/updated records count: " + ids.size()); // 3
+    //
+    //    TextSegment segment4 = TextSegment.from("John Lennon was a very cool person.");
+    //    Embedding embedding4 = embeddingModel.embed(segment4);
+    //    String s4id = embeddingStore.add(embedding4, segment4);
+    //
+    //    System.out.println("segment 4 id: " + s4id);
 
-    TextSegment segment4 = TextSegment.from("John Lennon was a very cool person.");
-    Embedding embedding4 = embeddingModel.embed(segment4);
-    String s4id = embeddingStore.add(embedding4, segment4);
+    Embedding queryEmbedding = embeddingModel.embed(
+      "What is your favorite sport?"
+    );
+    List<EmbeddingMatch<TextSegment>> relevant = embeddingStore.findRelevant(
+      queryEmbedding,
+      2
+    );
 
-    System.out.println("segment 4 id: " + s4id);
-
-    Embedding queryEmbedding = embeddingModel.embed("What is your favorite sport?");
-    List<EmbeddingMatch<TextSegment>> relevant = embeddingStore.findRelevant(queryEmbedding, 2);
-
-    System.out.println("relevant results count for sport question: " + relevant.size()); // 2
+    System.out.println(
+      "relevant results count for sport question: " + relevant.size()
+    ); // 2
 
     System.out.println(relevant.get(0).score()); // 0.639...
     System.out.println(relevant.get(0).embedded().text()); // football
@@ -73,7 +91,9 @@ public class VespaEmbeddingStoreExample {
     queryEmbedding = embeddingModel.embed("And what about musicians?");
     relevant = embeddingStore.findRelevant(queryEmbedding, 5, 0.3);
 
-    System.out.println("relevant results count for music question: " + relevant.size()); // 1
+    System.out.println(
+      "relevant results count for music question: " + relevant.size()
+    ); // 1
 
     System.out.println(relevant.get(0).score()); // 0.359...
     System.out.println(relevant.get(0).embedded().text()); // John Lennon
