55da2939e3376a3b66762f7ab2b3391c915d828d
Heezer <33568148+Heezer@users.noreply.github.com>
2023-09-29 14:36:42 +0200

    Vespa support (#8)

---
+++ other-examples/src/main/java/embedding/store/vespa/README.md
@@ -0,0 +1,74 @@
+# Server side setup with Maven to run example on Vespa Cloud
+
+1. Do steps 1 - 4 from https://cloud.vespa.ai/en/getting-started-java.html
+2. Create new Maven Java application and go to it
+3. Add following dependency to `pom.xml`, so the latest Vespa release is used on each build:
+
+```xml
+
+<parent>
+    <groupId>com.yahoo.vespa</groupId>
+    <artifactId>cloud-tenant-base</artifactId>
+    <version>[8,9)</version>
+    <relativePath/>
+</parent>
+```
+
+4. Create `src/main/application/schemas/langchain4j.sd` with following content:
+
+```sd
+schema langchain4j {
+  document langchain4j {
+    field text_segment type string {
+      indexing: summary | index
+    }
+    field vector type tensor<float>(x[384]) {
+      indexing: summary | attribute
+      attribute {
+        distance-metric: prenormalized-angular
+      }
+    }
+  }
+
+  rank-profile cosine_similarity {
+    inputs {
+      query(q) tensor<float>(x[384])
+    }
+    function cosine() {
+      expression: 1.0 - distance(field, vector)
+    }
+    first-phase {
+      expression: if (cosine() < query(threshold), -1, cosine())
+      rank-score-drop-limit: 0.0
+    }
+  }
+}
+```
+
+5. Create `src/main/application/services.xml` with following content:
+
+```xml
+<?xml version="1.0" encoding="utf-8" ?>
+<services version="1.0">
+    <container version="1.0" id="default">
+        <document-api/>
+        <search/>
+    </container>
+
+    <content id="langchain4j" version="1.0">
+        <redundancy>1</redundancy>
+        <documents>
+            <document type="langchain4j" mode="index"/>
+        </documents>
+    </content>
+</services>
+```
+
+6. Do steps 6 from https://cloud.vespa.ai/en/getting-started-java.html. This will generate SSL private key and
+   public certificate and store them locally. Their path will be used in client example. Certificate will be added
+   to the project under `src/main/application/security/clients.pem`.
+
+7. Now you can build (you need Java > 17 for this) and deploy your application: do steps 7 & 8 from
+   https://cloud.vespa.ai/en/getting-started.html.
+   Alternatively you can deploy your build application by using [Cloud UI](https://console.vespa-cloud.com) and
+   uploading `target/application.zip` there.
---
+++ other-examples/src/main/java/embedding/store/vespa/VespaEmbeddingStoreExample.java
@@ -0,0 +1,94 @@
+package embedding.store.vespa;
+
+import static dev.langchain4j.model.inprocess.InProcessEmbeddingModelType.ALL_MINILM_L6_V2;
+import static java.util.Arrays.asList;
+
+import dev.langchain4j.data.embedding.Embedding;
+import dev.langchain4j.data.segment.TextSegment;
+import dev.langchain4j.model.inprocess.InProcessEmbeddingModel;
+import dev.langchain4j.store.embedding.EmbeddingMatch;
+import dev.langchain4j.store.embedding.EmbeddingStore;
+import dev.langchain4j.store.embedding.vespa.VespaEmbeddingStore;
+import java.util.List;
+
+/**
+ * Example of integration with Vespa. You need to configure Vespa server side first, instructions are
+ * inside of README.md file.
+ * Requires "langchain4j-vespa" Maven/Gradle dependency
+ */
+public class VespaEmbeddingStoreExample {
+
+  public static void main(String[] args) {
+    EmbeddingStore<TextSegment> embeddingStore = VespaEmbeddingStore
+      .builder()
+      // server url, e.g. https://alexey-heezer.langchain4j.mytenant346.aws-us-east-1c.dev.z.vespa-app.cloud
+      .url("url")
+      // local path to the SSL private key file,
+      // e.g. /Users/user/.vespa/mytenant346.langchain4j.alexey-heezer/data-plane-private-key.pem
+      .keyPath("keyPath")
+      // local path to the SSL certificate file,
+      // e.g. /Users/user/.vespa/mytenant346.langchain4j.alexey-heezer/data-plane-public-cert.pem
+      .certPath("certPath")
+      .build();
+
+    InProcessEmbeddingModel embeddingModel = new InProcessEmbeddingModel(
+      ALL_MINILM_L6_V2
+    );
+
+    TextSegment segment1 = TextSegment.from("I like football.");
+    Embedding embedding1 = embeddingModel.embed(segment1);
+    embeddingStore.add(embedding1, segment1);
+
+    TextSegment segment2 = TextSegment.from("I've never been to New York.");
+    Embedding embedding2 = embeddingModel.embed(segment2);
+    embeddingStore.add(embedding2, segment2);
+
+    TextSegment segment3 = TextSegment.from(
+      "But actually we tried our new swimming pool yesterday and it was awesome!"
+    );
+    Embedding embedding3 = embeddingModel.embed(segment3);
+    embeddingStore.add(embedding3, segment3);
+
+    List<String> ids = embeddingStore.addAll(
+      asList(embedding1, embedding2, embedding3),
+      asList(segment1, segment2, segment3)
+    );
+
+    System.out.println("added/updated records count: " + ids.size()); // 3
+
+    TextSegment segment4 = TextSegment.from(
+      "John Lennon was a very cool person."
+    );
+    Embedding embedding4 = embeddingModel.embed(segment4);
+    String s4id = embeddingStore.add(embedding4, segment4);
+
+    System.out.println("segment 4 id: " + s4id);
+
+    Embedding queryEmbedding = embeddingModel.embed(
+      "What is your favorite sport?"
+    );
+    List<EmbeddingMatch<TextSegment>> relevant = embeddingStore.findRelevant(
+      queryEmbedding,
+      2
+    );
+
+    System.out.println(
+      "relevant results count for sport question: " + relevant.size()
+    ); // 2
+
+    System.out.println(relevant.get(0).score()); // 0.639...
+    System.out.println(relevant.get(0).embedded().text()); // football
+    System.out.println(relevant.get(1).score()); // 0.232...
+    System.out.println(relevant.get(1).embedded().text()); // swimming pool
+
+    queryEmbedding = embeddingModel.embed("And what about musicians?");
+    relevant = embeddingStore.findRelevant(queryEmbedding, 5, 0.3);
+
+    System.out.println(
+      "relevant results count for music question: " + relevant.size()
+    ); // 1
+
+    System.out.println(relevant.get(0).score()); // 0.359...
+    System.out.println(relevant.get(0).embedded().text()); // John Lennon
+  }
+}
