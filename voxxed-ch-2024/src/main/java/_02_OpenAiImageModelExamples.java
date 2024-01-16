4a8ebe29295d060d2cd606a06b159c7cb776e888
LizeRaes <49833622+LizeRaes@users.noreply.github.com>
2024-01-16 21:40:51 +0100

    examples for voxxeddays ch 2024

---
+++ voxxed-ch-2024/src/main/java/_02_OpenAiImageModelExamples.java
@@ -0,0 +1,99 @@
+import dev.langchain4j.chain.ConversationalRetrievalChain;
+import dev.langchain4j.data.document.Document;
+import dev.langchain4j.data.document.parser.TextDocumentParser;
+import dev.langchain4j.data.document.splitter.DocumentSplitters;
+import dev.langchain4j.data.image.Image;
+import dev.langchain4j.data.segment.TextSegment;
+import dev.langchain4j.model.embedding.AllMiniLmL6V2EmbeddingModel;
+import dev.langchain4j.model.embedding.EmbeddingModel;
+import dev.langchain4j.model.image.ImageModel;
+import dev.langchain4j.model.input.PromptTemplate;
+import dev.langchain4j.model.openai.OpenAiChatModel;
+import dev.langchain4j.model.openai.OpenAiImageModel;
+import dev.langchain4j.model.openai.OpenAiModelName;
+import dev.langchain4j.model.output.Response;
+import dev.langchain4j.retriever.EmbeddingStoreRetriever;
+import dev.langchain4j.store.embedding.EmbeddingStore;
+import dev.langchain4j.store.embedding.EmbeddingStoreIngestor;
+import dev.langchain4j.store.embedding.inmemory.InMemoryEmbeddingStore;
+
+import java.net.URISyntaxException;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+import static dev.ai4j.openai4j.image.ImageModel.DALL_E_QUALITY_HD;
+import static dev.langchain4j.data.document.loader.FileSystemDocumentLoader.loadDocument;
+
+public class _02_OpenAiImageModelExamples {
+
+    static class Simple_Prompt {
+
+        public static void main(String[] args) {
+
+            ImageModel model = OpenAiImageModel.builder().modelName(OpenAiModelName.DALL_E_2).apiKey(System.getenv(
+                    "OPENAI_API_KEY")).build();
+
+            Response<Image> response = model.generate("Swiss software developers with cheese fondue, a parrot and a " +
+                    "cup of coffee");
+
+            System.out.println(response.content().url()); // Donald Duck is here :)
+        }
+    }
+
+    static class Draw_Story_From_My_Document {
+
+        public static void main(String[] args) throws URISyntaxException {
+
+            ImageModel model = OpenAiImageModel.builder()
+                    .apiKey(System.getenv("OPENAI_API_KEY"))
+                    .quality(DALL_E_QUALITY_HD)
+                    .logRequests(true)
+                    .logResponses(true)
+                    .withPersisting()
+                    .build();
+
+            EmbeddingModel embeddingModel = new AllMiniLmL6V2EmbeddingModel();
+            EmbeddingStore<TextSegment> embeddingStore = new InMemoryEmbeddingStore<>();
+
+            EmbeddingStoreIngestor ingestor = EmbeddingStoreIngestor
+                    .builder()
+                    .documentSplitter(DocumentSplitters.recursive(1000, 0))
+                    .embeddingModel(embeddingModel)
+                    .embeddingStore(embeddingStore)
+                    .build();
+
+            Document document = loadDocument(
+                    Paths.get(
+                            Objects
+                                    .requireNonNull(
+                                            _02_OpenAiImageModelExamples.class.getResource("example-files/story-about" +
+                                                    "-happy-carrot.txt")
+                                    )
+                                    .toURI()
+                    ),
+                    new TextDocumentParser()
+            );
+            ingestor.ingest(document);
+
+            ConversationalRetrievalChain chain = ConversationalRetrievalChain
+                    .builder()
+                    .chatLanguageModel(OpenAiChatModel.builder().apiKey(System.getenv("OPENAI_API_KEY")).build())
+                    .retriever(EmbeddingStoreRetriever.from(embeddingStore, embeddingModel))
+                    .build();
+
+            PromptTemplate drawPromptTemplate = PromptTemplate.from(
+                    "Draw {{object}}. Base the picture on following information:\n\n{{information}}"
+            );
+
+            Map<String, Object> variables = new HashMap<>();
+            variables.put("information", chain.execute("Who is Charlie?"));
+            variables.put("object", "Ultra realistic Charlie on the party, cinematic lighting");
+
+            Response<Image> response = model.generate(drawPromptTemplate.apply(variables).text());
+
+            System.out.println(response.content().url()); // Enjoy your locally stored picture of Charlie on the party :)
+        }
+    }
+}
---
+++ voxxed-ch-2024/src/main/java/_08_AIServiceExamples.java
@@ -0,0 +1,379 @@
+import dev.langchain4j.memory.ChatMemory;
+import dev.langchain4j.memory.chat.MessageWindowChatMemory;
+import dev.langchain4j.model.chat.ChatLanguageModel;
+import dev.langchain4j.model.input.structured.StructuredPrompt;
+import dev.langchain4j.model.openai.OpenAiChatModel;
+import dev.langchain4j.model.output.structured.Description;
+import dev.langchain4j.service.AiServices;
+import dev.langchain4j.service.MemoryId;
+import dev.langchain4j.service.SystemMessage;
+import dev.langchain4j.service.UserMessage;
+import dev.langchain4j.service.V;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
+
+import static java.time.Duration.ofSeconds;
+import static java.util.Arrays.asList;
+
+public class _08_AIServiceExamples {
+
+    static ChatLanguageModel model = OpenAiChatModel.builder()
+            .apiKey(ApiKeys.OPENAI_API_KEY)
+            .timeout(ofSeconds(60))
+            .build();
+
+    ////////////////// SIMPLE EXAMPLE //////////////////////
+
+    static class Simple_AI_Service_Example {
+
+        interface Assistant {
+
+            String chat(String message);
+        }
+
+        public static void main(String[] args) {
+
+            Assistant assistant = AiServices.create(Assistant.class, model);
+
+            String userMessage = "Translate 'Plus-Values des cessions de valeurs mobilières, de droits sociaux et gains assimilés'";
+
+            String answer = assistant.chat(userMessage);
+
+            System.out.println(answer);
+        }
+    }
+
+    ////////////////// WITH MESSAGE AND VARIABLES //////////////////////
+
+    static class AI_Service_with_System_Message_Example {
+
+        interface Chef {
+
+            @SystemMessage("You are a professional chef. You are friendly, polite and concise.")
+            String answer(String question);
+        }
+
+        public static void main(String[] args) {
+
+            Chef chef = AiServices.create(Chef.class, model);
+
+            String answer = chef.answer("How long should I grill chicken?");
+
+            System.out.println(answer); // Grilling chicken usually takes around 10-15 minutes per side ...
+        }
+    }
+
+    static class AI_Service_with_System_and_User_Messages_Example {
+
+        interface TextUtils {
+
+            @SystemMessage("You are a professional translator into {{language}}")
+            @UserMessage("Translate the following text: {{text}}")
+            String translate(@V("text") String text, @V("language") String language);
+
+            @SystemMessage("Summarize every message from user in {{n}} bullet points. Provide only bullet points.")
+            List<String> summarize(@UserMessage String text, @V("n") int n);
+        }
+
+        public static void main(String[] args) {
+
+            TextUtils utils = AiServices.create(TextUtils.class, model);
+
+            String translation = utils.translate("Hello, how are you?", "italian");
+            System.out.println(translation); // Ciao, come stai?
+
+            String text = "AI, or artificial intelligence, is a branch of computer science that aims to create "
+                    + "machines that mimic human intelligence. This can range from simple tasks such as recognizing "
+                    + "patterns or speech to more complex tasks like making decisions or predictions.";
+
+            List<String> bulletPoints = utils.summarize(text, 3);
+            System.out.println(bulletPoints);
+            // [
+            // "- AI is a branch of computer science",
+            // "- It aims to create machines that mimic human intelligence",
+            // "- It can perform simple or complex tasks"
+            // ]
+        }
+    }
+
+    //////////////////// EXTRACTING DIFFERENT DATA TYPES ////////////////////
+
+    static class Sentiment_Extracting_AI_Service_Example {
+
+        enum Sentiment {
+            POSITIVE, NEUTRAL, NEGATIVE;
+        }
+
+        interface SentimentAnalyzer {
+
+            @UserMessage("Analyze sentiment of {{it}}")
+            Sentiment analyzeSentimentOf(String text);
+
+            @UserMessage("Does {{it}} have a positive sentiment?")
+            boolean isPositive(String text);
+        }
+
+        public static void main(String[] args) {
+
+            SentimentAnalyzer sentimentAnalyzer = AiServices.create(SentimentAnalyzer.class, model);
+
+            Sentiment sentiment = sentimentAnalyzer.analyzeSentimentOf("It is good!");
+            System.out.println(sentiment); // POSITIVE
+
+            boolean positive = sentimentAnalyzer.isPositive("It is bad!");
+            System.out.println(positive); // false
+        }
+    }
+
+    static class Number_Extracting_AI_Service_Example {
+
+        interface NumberExtractor {
+
+            @UserMessage("Extract number from {{it}}")
+            int extractInt(String text);
+
+            @UserMessage("Extract number from {{it}}")
+            long extractLong(String text);
+
+            @UserMessage("Extract number from {{it}}")
+            BigInteger extractBigInteger(String text);
+
+            @UserMessage("Extract number from {{it}}")
+            float extractFloat(String text);
+
+            @UserMessage("Extract number from {{it}}")
+            double extractDouble(String text);
+
+            @UserMessage("Extract number from {{it}}")
+            BigDecimal extractBigDecimal(String text);
+        }
+
+        public static void main(String[] args) {
+
+            NumberExtractor extractor = AiServices.create(NumberExtractor.class, model);
+
+            String text = "After countless millennia of computation, the supercomputer Deep Thought finally announced "
+                    + "that the answer to the ultimate question of life, the universe, and everything was forty two.";
+
+            int intNumber = extractor.extractInt(text);
+            System.out.println(intNumber); // 42
+
+            long longNumber = extractor.extractLong(text);
+            System.out.println(longNumber); // 42
+
+            BigInteger bigIntegerNumber = extractor.extractBigInteger(text);
+            System.out.println(bigIntegerNumber); // 42
+
+            float floatNumber = extractor.extractFloat(text);
+            System.out.println(floatNumber); // 42.0
+
+            double doubleNumber = extractor.extractDouble(text);
+            System.out.println(doubleNumber); // 42.0
+
+            BigDecimal bigDecimalNumber = extractor.extractBigDecimal(text);
+            System.out.println(bigDecimalNumber); // 42.0
+        }
+    }
+
+    static class Date_and_Time_Extracting_AI_Service_Example {
+
+        interface DateTimeExtractor {
+
+            @UserMessage("Extract date from {{it}}")
+            LocalDate extractDateFrom(String text);
+
+            @UserMessage("Extract time from {{it}}")
+            LocalTime extractTimeFrom(String text);
+
+            @UserMessage("Extract date and time from {{it}}")
+            LocalDateTime extractDateTimeFrom(String text);
+        }
+
+        public static void main(String[] args) {
+
+            DateTimeExtractor extractor = AiServices.create(DateTimeExtractor.class, model);
+
+            String text = "The tranquility pervaded the evening of 1968, just fifteen minutes shy of midnight,"
+                    + " following the celebrations of Independence Day.";
+
+            LocalDate date = extractor.extractDateFrom(text);
+            System.out.println(date); // 1968-07-04
+
+            LocalTime time = extractor.extractTimeFrom(text);
+            System.out.println(time); // 23:45
+
+            LocalDateTime dateTime = extractor.extractDateTimeFrom(text);
+            System.out.println(dateTime); // 1968-07-04T23:45
+        }
+    }
+
+    static class POJO_Extracting_AI_Service_Example {
+
+        static class Person {
+
+            private String firstName;
+            private String lastName;
+            private LocalDate birthDate;
+
+            @Override
+            public String toString() {
+                return "Person {" +
+                        " firstName = \"" + firstName + "\"" +
+                        ", lastName = \"" + lastName + "\"" +
+                        ", birthDate = " + birthDate +
+                        " }";
+            }
+        }
+
+        interface PersonExtractor {
+
+            @UserMessage("Extract information about a person from {{it}}")
+            Person extractPersonFrom(String text);
+        }
+
+        public static void main(String[] args) {
+
+            PersonExtractor extractor = AiServices.create(PersonExtractor.class, model);
+
+            String text = "In 1968, amidst the fading echoes of Independence Day, "
+                    + "a child named John arrived under the calm evening sky. "
+                    + "This newborn, bearing the surname Doe, marked the start of a new journey.";
+
+            Person person = extractor.extractPersonFrom(text);
+
+            System.out.println(person); // Person { firstName = "John", lastName = "Doe", birthDate = 1968-07-04 }
+        }
+    }
+
+    ////////////////////// DESCRIPTIONS ////////////////////////
+
+    static class POJO_With_Descriptions_Extracting_AI_Service_Example {
+
+        static class Recipe {
+
+            @Description("short title, 3 words maximum")
+            private String title;
+
+            @Description("short description, 2 sentences maximum")
+            private String description;
+
+            @Description("each step should be described in 6 to 8 words, steps should rhyme with each other")
+            private List<String> steps;
+
+            private Integer preparationTimeMinutes;
+
+            @Override
+            public String toString() {
+                return "Recipe {" +
+                        " title = \"" + title + "\"" +
+                        ", description = \"" + description + "\"" +
+                        ", steps = " + steps +
+                        ", preparationTimeMinutes = " + preparationTimeMinutes +
+                        " }";
+            }
+        }
+
+        @StructuredPrompt("Create a recipe of a {{dish}} that can be prepared using only {{ingredients}}")
+        static class CreateRecipePrompt {
+
+            private String dish;
+            private List<String> ingredients;
+        }
+
+        interface Chef {
+
+            Recipe createRecipeFrom(String... ingredients);
+
+            Recipe createRecipe(CreateRecipePrompt prompt);
+        }
+
+        public static void main(String[] args) {
+
+            Chef chef = AiServices.create(Chef.class, model);
+
+            Recipe recipe = chef.createRecipeFrom("cucumber", "tomato", "feta", "onion", "olives", "lemon");
+
+            System.out.println(recipe);
+            // Recipe {
+            // title = "Greek Salad",
+            // description = "A refreshing mix of veggies and feta cheese in a zesty
+            // dressing.",
+            // steps = [
+            // "Chop cucumber and tomato",
+            // "Add onion and olives",
+            // "Crumble feta on top",
+            // "Drizzle with dressing and enjoy!"
+            // ],
+            // preparationTimeMinutes = 10
+            // }
+
+            CreateRecipePrompt prompt = new CreateRecipePrompt();
+            prompt.dish = "oven dish";
+            prompt.ingredients = asList("cucumber", "tomato", "feta", "onion", "olives", "potatoes");
+
+            Recipe anotherRecipe = chef.createRecipe(prompt);
+            System.out.println(anotherRecipe);
+            // Recipe ...
+        }
+    }
+
+
+    ////////////////////////// WITH MEMORY /////////////////////////
+
+    static class ServiceWithMemoryExample {
+
+        interface Assistant {
+
+            String chat(String message);
+        }
+
+        public static void main(String[] args) {
+
+            ChatMemory chatMemory = MessageWindowChatMemory.withMaxMessages(10);
+
+            Assistant assistant = AiServices.builder(Assistant.class)
+                    .chatLanguageModel(model)
+                    .chatMemory(chatMemory)
+                    .build();
+
+            String answer = assistant.chat("Hello! My name is Klaus.");
+            System.out.println(answer); // Hello Klaus! How can I assist you today?
+
+            String answerWithName = assistant.chat("What is my name?");
+            System.out.println(answerWithName); // Your name is Klaus.
+        }
+    }
+
+    static class ServiceWithMemoryForEachUserExample {
+
+        interface Assistant {
+
+            String chat(@MemoryId int memoryId, @UserMessage String userMessage);
+        }
+
+        public static void main(String[] args) {
+
+            Assistant assistant = AiServices.builder(Assistant.class)
+                    .chatLanguageModel(model)
+                    .chatMemoryProvider(memoryId -> MessageWindowChatMemory.withMaxMessages(10))
+                    .build();
+
+            System.out.println(assistant.chat(1, "Hello, my name is Klaus"));
+            // Hi Klaus! How can I assist you today?
+
+            System.out.println(assistant.chat(2, "Hello, my name is Francine"));
+            // Hello Francine! How can I assist you today?
+
+            System.out.println(assistant.chat(1, "What is my name?"));
+            // Your name is Klaus.
+
+            System.out.println(assistant.chat(2, "What is my name?"));
+            // Your name is Francine.
+        }
+    }
+}
