46220d9de2421cfba4ab47555fd89b23c027204e
Lu Han <feuyeux@gmail.com>
2024-02-06 17:28:13 +0800

    unify the coding style of using testcontainers

--- neo4j-example/src/main/java/Neo4jEmbeddingStoreExample.java
+++ neo4j-example/src/main/java/Neo4jEmbeddingStoreExample.java
@@ -5,17 +5,36 @@ import dev.langchain4j.model.embedding.EmbeddingModel;
 import dev.langchain4j.store.embedding.EmbeddingMatch;
 import dev.langchain4j.store.embedding.EmbeddingStore;
 import dev.langchain4j.store.embedding.neo4j.Neo4jEmbeddingStore;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.neo4j.driver.AuthTokens;
+import org.neo4j.driver.GraphDatabase;
 import org.testcontainers.containers.Neo4jContainer;
+import org.testcontainers.utility.DockerImageName;
 
 import java.util.List;
 
 public class Neo4jEmbeddingStoreExample {
 
+    private static final Logger LOGGER = LogManager.getLogger(Neo4jEmbeddingStoreExample.class);
+
+    /**
+     * To run this example, ensure you have Neo4j running locally,
+     * and change uri, username and password strings consistently.
+     * If not, then:
+     * - Execute "docker pull neo4j:latest"
+     * - Execute "docker run -d -p 7687:7687 --env NEO4J_AUTH=neo4j/password1234 neo4j:latest"
+     * - Wait until Neo4j is ready to serve (may take a few minutes)
+     */
+
     public static void main(String[] args) {
-        try (Neo4jContainer<?> neo4j = new Neo4jContainer<>("neo4j:5")) {
-            neo4j.start();
+        String username = "neo4j";
+        String password = "password1234";
+
+        try (Neo4jContainer<?> neo4jContainer = new Neo4jContainer<>(DockerImageName.parse("neo4j:latest"))) {
+            neo4jContainer.withEnv("NEO4J_AUTH", username + "/" + password).start();
             EmbeddingStore<TextSegment> embeddingStore = Neo4jEmbeddingStore.builder()
-                    .withBasicAuth(neo4j.getBoltUrl(), "neo4j", neo4j.getAdminPassword())
+                    .driver(GraphDatabase.driver(neo4jContainer.getBoltUrl(), AuthTokens.basic(username, password)))
                     .dimension(384)
                     .build();
 
@@ -33,8 +52,12 @@ public class Neo4jEmbeddingStoreExample {
             List<EmbeddingMatch<TextSegment>> relevant = embeddingStore.findRelevant(queryEmbedding, 1);
             EmbeddingMatch<TextSegment> embeddingMatch = relevant.get(0);
 
-            System.out.println(embeddingMatch.score()); // 0.8144289255142212
-            System.out.println(embeddingMatch.embedded().text()); // I like football.
+            // expected 0.8144288659095
+            LOGGER.info("Score: {}", embeddingMatch.score());
+            // expected "I like football."
+            LOGGER.info("Embedded: {}", embeddingMatch.embedded().text());
+        } catch (Exception e) {
+            LOGGER.error("Error: {}", e.getMessage());
         }
     }
 }
