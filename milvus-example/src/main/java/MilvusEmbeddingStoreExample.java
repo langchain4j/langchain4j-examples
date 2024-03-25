7b1e5c34af2763c98abea494be4b07ce2379abfd
Eddú Meléndez <eddu.melendez@gmail.com>
2024-03-25 12:45:24 -0600

    Use Testcontainers Milvus module and fix example

--- milvus-example/src/main/java/MilvusEmbeddingStoreExample.java
+++ milvus-example/src/main/java/MilvusEmbeddingStoreExample.java
@@ -5,40 +5,38 @@ import dev.langchain4j.model.embedding.EmbeddingModel;
 import dev.langchain4j.store.embedding.EmbeddingMatch;
 import dev.langchain4j.store.embedding.EmbeddingStore;
 import dev.langchain4j.store.embedding.milvus.MilvusEmbeddingStore;
+import org.testcontainers.milvus.MilvusContainer;
 
 import java.util.List;
 
 public class MilvusEmbeddingStoreExample {
 
-    /**
-     * First run Milvus locally:
-     * Run "docker compose up -d" inside "../resources" directory.
-     * If you want to create a fresh Milvus instance, don't forget to remove "../resources/volumes" directory.
-     */
-
     public static void main(String[] args) {
 
-        EmbeddingStore<TextSegment> embeddingStore = MilvusEmbeddingStore.builder()
-                .host("localhost")
-                .port(19530)
-                .dimension(384)
-                .build();
+        try (MilvusContainer milvus = new MilvusContainer("milvusdb/milvus:v2.3.1")) {
+            milvus.start();
+            EmbeddingStore<TextSegment> embeddingStore = MilvusEmbeddingStore.builder()
+                    .uri(milvus.getEndpoint())
+                    .collectionName("test_collection")
+                    .dimension(384)
+                    .build();
 
-        EmbeddingModel embeddingModel = new AllMiniLmL6V2EmbeddingModel();
+            EmbeddingModel embeddingModel = new AllMiniLmL6V2EmbeddingModel();
 
-        TextSegment segment1 = TextSegment.from("I like football.");
-        Embedding embedding1 = embeddingModel.embed(segment1).content();
-        embeddingStore.add(embedding1, segment1);
+            TextSegment segment1 = TextSegment.from("I like football.");
+            Embedding embedding1 = embeddingModel.embed(segment1).content();
+            embeddingStore.add(embedding1, segment1);
 
-        TextSegment segment2 = TextSegment.from("The weather is good today.");
-        Embedding embedding2 = embeddingModel.embed(segment2).content();
-        embeddingStore.add(embedding2, segment2);
+            TextSegment segment2 = TextSegment.from("The weather is good today.");
+            Embedding embedding2 = embeddingModel.embed(segment2).content();
+            embeddingStore.add(embedding2, segment2);
 
-        Embedding queryEmbedding = embeddingModel.embed("What is your favourite sport?").content();
-        List<EmbeddingMatch<TextSegment>> relevant = embeddingStore.findRelevant(queryEmbedding, 1);
-        EmbeddingMatch<TextSegment> embeddingMatch = relevant.get(0);
+            Embedding queryEmbedding = embeddingModel.embed("What is your favourite sport?").content();
+            List<EmbeddingMatch<TextSegment>> relevant = embeddingStore.findRelevant(queryEmbedding, 1);
+            EmbeddingMatch<TextSegment> embeddingMatch = relevant.get(0);
 
-        System.out.println(embeddingMatch.score()); // 0.8144287765026093
-        System.out.println(embeddingMatch.embedded().text()); // I like football.
+            System.out.println(embeddingMatch.score()); // 0.8144287765026093
+            System.out.println(embeddingMatch.embedded().text()); // I like football.
+        }
     }
 }
