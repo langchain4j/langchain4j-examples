71eb4d55982bc5c69c641d9b930101683c504d44
LangChain4j <info@langchain4j.dev>
2024-03-26 15:14:44 +0100

    updated Spring Boot example

--- spring-boot-example/src/main/java/dev/langchain4j/example/aiservice/Assistant.java
+++ spring-boot-example/src/main/java/dev/langchain4j/example/aiservice/Assistant.java
@@ -1,4 +1,4 @@
-package dev.langchain4j.example;
+package dev.langchain4j.example.aiservice;
 
 import dev.langchain4j.service.SystemMessage;
 import dev.langchain4j.service.spring.AiService;
@@ -6,6 +6,6 @@ import dev.langchain4j.service.spring.AiService;
 @AiService
 interface Assistant {
 
-    @SystemMessage("You are a sarcastic assistant")
+    @SystemMessage("You are a polite assistant")
     String chat(String userMessage);
 }
---
+++ spring-boot-example/src/main/java/dev/langchain4j/example/aiservice/AssistantConfiguration.java
@@ -0,0 +1,18 @@
+package dev.langchain4j.example.aiservice;
+
+import dev.langchain4j.memory.ChatMemory;
+import dev.langchain4j.memory.chat.MessageWindowChatMemory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+class AssistantConfiguration {
+
+    /**
+     * This chat memory will be used by an {@link Assistant}
+     */
+    @Bean
+    ChatMemory chatMemory() {
+        return MessageWindowChatMemory.withMaxMessages(10);
+    }
+}
