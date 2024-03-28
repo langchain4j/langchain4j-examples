5a19b723661530cf64846a256e2b01b060e7fb0b
LangChain4j <info@langchain4j.dev>
2024-03-28 17:39:05 +0100

    updated to 0.29.1

--- spring-boot-example/src/main/java/dev/langchain4j/example/lowlevel/ChatLanguageModelController.java
+++ spring-boot-example/src/main/java/dev/langchain4j/example/lowlevel/ChatLanguageModelController.java
@@ -1,20 +1,21 @@
 package dev.langchain4j.example.lowlevel;
 
 import dev.langchain4j.model.chat.ChatLanguageModel;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * This is an example of using a {@link ChatLanguageModel}, a low-level LangChain4j API.
+ */
 @RestController
 class ChatLanguageModelController {
 
-    /**
-     * This is an example of using a {@link ChatLanguageModel}, a low-level LangChain4j API
-     */
-
-    @Autowired
     ChatLanguageModel chatLanguageModel;
+
+    ChatLanguageModelController(ChatLanguageModel chatLanguageModel) {
+        this.chatLanguageModel = chatLanguageModel;
+    }
 
     @GetMapping("/model")
     public String model(@RequestParam(value = "message", defaultValue = "Hello") String message) {
