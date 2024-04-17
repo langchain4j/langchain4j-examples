792e5161babd46a72b0ad89cc085efb51ee17c78
LangChain4j <info@langchain4j.dev>
2024-04-17 10:10:46 +0200

    Added _06_Advanced_RAG_Skip_Retrieval_Example

---
+++ rag-examples/src/main/java/_3_advanced/_06_Advanced_RAG_Skip_Retrieval_Example.java
@@ -0,0 +1,142 @@
+package _3_advanced;
+
+import _2_naive.Naive_RAG_Example;
+import dev.langchain4j.data.document.Document;
+import dev.langchain4j.data.document.DocumentParser;
+import dev.langchain4j.data.document.DocumentSplitter;
+import dev.langchain4j.data.document.parser.TextDocumentParser;
+import dev.langchain4j.data.document.splitter.DocumentSplitters;
+import dev.langchain4j.data.embedding.Embedding;
+import dev.langchain4j.data.message.AiMessage;
+import dev.langchain4j.data.segment.TextSegment;
+import dev.langchain4j.memory.chat.MessageWindowChatMemory;
+import dev.langchain4j.model.chat.ChatLanguageModel;
+import dev.langchain4j.model.embedding.EmbeddingModel;
+import dev.langchain4j.model.embedding.bge.small.en.v15.BgeSmallEnV15QuantizedEmbeddingModel;
+import dev.langchain4j.model.input.Prompt;
+import dev.langchain4j.model.input.PromptTemplate;
+import dev.langchain4j.model.openai.OpenAiChatModel;
+import dev.langchain4j.rag.DefaultRetrievalAugmentor;
+import dev.langchain4j.rag.RetrievalAugmentor;
+import dev.langchain4j.rag.content.retriever.ContentRetriever;
+import dev.langchain4j.rag.content.retriever.EmbeddingStoreContentRetriever;
+import dev.langchain4j.rag.query.Query;
+import dev.langchain4j.rag.query.router.QueryRouter;
+import dev.langchain4j.service.AiServices;
+import dev.langchain4j.store.embedding.EmbeddingStore;
+import dev.langchain4j.store.embedding.inmemory.InMemoryEmbeddingStore;
+import shared.Assistant;
+
+import java.nio.file.Path;
+import java.util.Collection;
+import java.util.List;
+
+import static dev.langchain4j.data.document.loader.FileSystemDocumentLoader.loadDocument;
+import static java.util.Collections.emptyList;
+import static java.util.Collections.singletonList;
+import static shared.Utils.*;
+
+public class _06_Advanced_RAG_Skip_Retrieval_Example {
+
+
+    /**
+     * Please refer to {@link Naive_RAG_Example} for a basic context.
+     * <p>
+     * Advanced RAG in LangChain4j is described here: https://github.com/langchain4j/langchain4j/pull/538
+     * <p>
+     * This example demonstrates how to conditionally skip retrieval.
+     * Sometimes, retrieval is unnecessary, for instance, when a user simply says "Hi".
+     * <p>
+     * There are multiple ways to implement this, but the simplest one is to use a custom {@link QueryRouter}.
+     * When retrieval should be skipped, QueryRouter will return an empty list,
+     * meaning that the query will not be routed to any {@link ContentRetriever}.
+     * <p>
+     * Decision-making can be implemented in various ways:
+     * - Using rules (e.g., depending on the user's privileges, location, etc.).
+     * - Using keywords (e.g., if a query contains specific words).
+     * - Using semantic similarity (see EmbeddingModelTextClassifierExample in this repository).
+     * - Using an LLM to make a decision.
+     * <p>
+     * In this example, we will use an LLM to decide whether a user query should do retrieval or not.
+     */
+
+    public static void main(String[] args) {
+
+        Assistant assistant = createAssistant();
+
+        // First, say "Hi"
+        // Notice how this query is not routed to any retrievers.
+
+        // Now, ask "Can I cancel my reservation?"
+        // This query has been routed to our retriever.
+        startConversationWith(assistant);
+    }
+
+    private static Assistant createAssistant() {
+
+        EmbeddingModel embeddingModel = new BgeSmallEnV15QuantizedEmbeddingModel();
+
+        EmbeddingStore<TextSegment> embeddingStore =
+                embed(toPath("documents/miles-of-smiles-terms-of-use.txt"), embeddingModel);
+
+        ContentRetriever contentRetriever = EmbeddingStoreContentRetriever.builder()
+                .embeddingStore(embeddingStore)
+                .embeddingModel(embeddingModel)
+                .maxResults(2)
+                .minScore(0.6)
+                .build();
+
+        ChatLanguageModel chatLanguageModel = OpenAiChatModel.builder()
+                .apiKey(OPENAI_API_KEY)
+                .build();
+
+        // Let's create a query router.
+        QueryRouter queryRouter = new QueryRouter() {
+
+            private final PromptTemplate PROMPT_TEMPLATE = PromptTemplate.from(
+                    "Is the following query related to the business of the car rental company? " +
+                            "Answer only 'yes', 'no' or 'maybe'. " +
+                            "Query: {{it}}"
+            );
+
+            @Override
+            public Collection<ContentRetriever> route(Query query) {
+
+                Prompt prompt = PROMPT_TEMPLATE.apply(query.text());
+
+                AiMessage aiMessage = chatLanguageModel.generate(prompt.toUserMessage()).content();
+                System.out.println("LLM decided: " + aiMessage.text());
+
+                if (aiMessage.text().toLowerCase().contains("no")) {
+                    return emptyList();
+                }
+
+                return singletonList(contentRetriever);
+            }
+        };
+
+        RetrievalAugmentor retrievalAugmentor = DefaultRetrievalAugmentor.builder()
+                .queryRouter(queryRouter)
+                .build();
+
+        return AiServices.builder(Assistant.class)
+                .chatLanguageModel(chatLanguageModel)
+                .retrievalAugmentor(retrievalAugmentor)
+                .chatMemory(MessageWindowChatMemory.withMaxMessages(10))
+                .build();
+    }
+
+    private static EmbeddingStore<TextSegment> embed(Path documentPath, EmbeddingModel embeddingModel) {
+        DocumentParser documentParser = new TextDocumentParser();
+        Document document = loadDocument(documentPath, documentParser);
+
+        DocumentSplitter splitter = DocumentSplitters.recursive(300, 0);
+        List<TextSegment> segments = splitter.split(document);
+
+        List<Embedding> embeddings = embeddingModel.embedAll(segments).content();
+
+        EmbeddingStore<TextSegment> embeddingStore = new InMemoryEmbeddingStore<>();
+        embeddingStore.addAll(embeddings, segments);
+        return embeddingStore;
+    }
+}
