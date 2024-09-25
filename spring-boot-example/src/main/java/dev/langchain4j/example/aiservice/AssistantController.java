c0374e78f1bc615a6f21153a6215c84e491ef1f2
LangChain4j <info@langchain4j.dev>
2024-09-25 14:11:06 +0200

    Added example for Flux<String> support

--- spring-boot-example/src/main/java/dev/langchain4j/example/aiservice/AssistantController.java
+++ spring-boot-example/src/main/java/dev/langchain4j/example/aiservice/AssistantController.java
@@ -4,6 +4,7 @@ import dev.langchain4j.service.spring.AiService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 /**
  * This is an example of using an {@link AiService}, a high-level LangChain4j API.
@@ -12,13 +13,20 @@ import org.springframework.web.bind.annotation.RestController;
 class AssistantController {
 
     Assistant assistant;
+    StreamingAssistant streamingAssistant;
 
-    AssistantController(Assistant assistant) {
+    AssistantController(Assistant assistant, StreamingAssistant streamingAssistant) {
         this.assistant = assistant;
+        this.streamingAssistant = streamingAssistant;
     }
 
     @GetMapping("/assistant")
     public String assistant(@RequestParam(value = "message", defaultValue = "What is the time now?") String message) {
         return assistant.chat(message);
+    }
+
+    @GetMapping("/streamingAssistant")
+    public Flux<String> streamingAssistant(@RequestParam(value = "message", defaultValue = "What is the time now?") String message) {
+        return streamingAssistant.chat(message);
     }
 }
