9079b27f218146479e436d0045e14c9a1d812778
Jake Luciani <jake@apache.org>
2024-11-24 22:47:08 -0500

    Update Jlama examples

--- jlama-examples/src/main/java/JlamaStreamingChatModelExamples.java
+++ jlama-examples/src/main/java/JlamaStreamingChatModelExamples.java
@@ -18,7 +18,7 @@ public class JlamaStreamingChatModelExamples {
             CompletableFuture<Response<AiMessage>> futureResponse = new CompletableFuture<>();
 
             StreamingChatLanguageModel model = JlamaStreamingChatModel.builder()
-                    .modelName("tjake/TinyLlama-1.1B-Chat-v1.0-Jlama-Q4")
+                    .modelName("tjake/Llama-3.2-1B-Instruct-JQ4")
                     .temperature(0.3f)
                     .build();
 
