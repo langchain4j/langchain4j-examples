ee135e39ca08c49ac23d56b7e67a89e6c26df0d9
LangChain4j <info@langchain4j.dev>
2024-12-22 18:13:03 +0100

    OpenAI: added examples of the new chat model API

--- open-ai-examples/src/main/java/OpenAiChatModelExamples.java
+++ open-ai-examples/src/main/java/OpenAiChatModelExamples.java
@@ -3,8 +3,10 @@ import dev.langchain4j.data.message.TextContent;
 import dev.langchain4j.data.message.UserMessage;
 import dev.langchain4j.model.chat.ChatLanguageModel;
 import dev.langchain4j.model.chat.request.ChatRequest;
+import dev.langchain4j.model.chat.request.ChatRequestParameters;
 import dev.langchain4j.model.chat.response.ChatResponse;
 import dev.langchain4j.model.openai.OpenAiChatModel;
+import dev.langchain4j.model.openai.OpenAiChatRequestParameters;
 
 import static dev.langchain4j.model.openai.OpenAiChatModelName.GPT_4_O_MINI;
 
@@ -47,6 +49,76 @@ public class OpenAiChatModelExamples {
             ChatResponse chatResponse = chatModel.chat(chatRequest);
 
             System.out.println(chatResponse.aiMessage().text());
+        }
+    }
+
+    static class Setting_Common_ChatRequestParameters {
+
+        public static void main(String[] args) {
+
+            ChatRequestParameters defaultParameters = ChatRequestParameters.builder()
+                    .modelName("gpt-4o")
+                    .temperature(0.7)
+                    .maxOutputTokens(100)
+                    // there are many more common parameters, see ChatRequestParameters for more info
+                    .build();
+
+            ChatLanguageModel chatModel = OpenAiChatModel.builder()
+                    .apiKey(ApiKeys.OPENAI_API_KEY)
+                    .defaultRequestParameters(defaultParameters)
+                    .logRequests(true)
+                    .build();
+
+            ChatRequestParameters parameters = ChatRequestParameters.builder()
+                    .modelName("gpt-4o-mini")
+                    .temperature(1.0)
+                    .maxOutputTokens(50)
+                    .build();
+
+            ChatRequest chatRequest = ChatRequest.builder()
+                    .messages(UserMessage.from("Tell me a funny story about Java"))
+                    .parameters(parameters) // merges with and overrides default parameters
+                    .build();
+
+            ChatResponse chatResponse = chatModel.chat(chatRequest);
+
+            System.out.println(chatResponse);
+        }
+    }
+
+    static class Setting_OpenAI_Specific_ChatRequestParameters {
+
+        public static void main(String[] args) {
+
+            OpenAiChatRequestParameters defaultParameters = OpenAiChatRequestParameters.builder()
+                    .seed(12345) // OpenAI-specific parameter
+                    // there are many more OpenAI-specific parameters, see OpenAiChatRequestParameters for more info
+                    .modelName("gpt-4o") // common parameter
+                    .temperature(0.7) // common parameter
+                    .maxOutputTokens(100) // common parameter
+                    .build();
+
+            ChatLanguageModel chatModel = OpenAiChatModel.builder()
+                    .apiKey(ApiKeys.OPENAI_API_KEY)
+                    .defaultRequestParameters(defaultParameters)
+                    .logRequests(true)
+                    .build();
+
+            OpenAiChatRequestParameters parameters = OpenAiChatRequestParameters.builder()
+                    .seed(67890) // OpenAI-specific parameter
+                    .modelName("gpt-4o-mini") // common parameter
+                    .temperature(1.0) // common parameter
+                    .maxOutputTokens(50) // common parameter
+                    .build();
+
+            ChatRequest chatRequest = ChatRequest.builder()
+                    .messages(UserMessage.from("Tell me a funny story about Java"))
+                    .parameters(parameters) // merges with and overrides default parameters
+                    .build();
+
+            ChatResponse chatResponse = chatModel.chat(chatRequest);
+
+            System.out.println(chatResponse);
         }
     }
 }
