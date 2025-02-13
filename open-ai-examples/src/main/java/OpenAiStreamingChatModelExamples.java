dd34f76b15392617b88a6623e62ce3ec40b17c92
Dmytro Liubarskyi <ljubarskij@gmail.com>
2025-02-13 10:38:45 +0100

    WIP

--- open-ai-examples/src/main/java/OpenAiStreamingChatModelExamples.java
+++ open-ai-examples/src/main/java/OpenAiStreamingChatModelExamples.java
@@ -3,33 +3,39 @@ import dev.langchain4j.model.chat.response.ChatResponse;
 import dev.langchain4j.model.chat.response.StreamingChatResponseHandler;
 import dev.langchain4j.model.openai.OpenAiStreamingChatModel;
 
+import java.util.concurrent.CompletableFuture;
+
 import static dev.langchain4j.model.openai.OpenAiChatModelName.GPT_4_O_MINI;
 
 public class OpenAiStreamingChatModelExamples {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         StreamingChatLanguageModel chatModel = OpenAiStreamingChatModel.builder()
                 .apiKey(ApiKeys.OPENAI_API_KEY)
                 .modelName(GPT_4_O_MINI)
                 .build();
 
+        CompletableFuture<ChatResponse> futureChatResponse = new CompletableFuture<>();
+
         chatModel.chat("Tell me a joke about Java", new StreamingChatResponseHandler() {
 
             @Override
             public void onPartialResponse(String partialResponse) {
-                System.out.println("onPartialResponse(): " + partialResponse);
+                System.out.print(partialResponse);
             }
 
             @Override
             public void onCompleteResponse(ChatResponse completeResponse) {
-                System.out.println("onCompleteResponse(): " + completeResponse);
+                futureChatResponse.complete(completeResponse);
             }
 
             @Override
             public void onError(Throwable error) {
-                error.printStackTrace();
+                futureChatResponse.completeExceptionally(error);
             }
         });
+
+        futureChatResponse.join();
     }
 }
