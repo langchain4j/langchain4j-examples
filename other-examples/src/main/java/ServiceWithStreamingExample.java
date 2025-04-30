8babc07dbad95504493f4723c34c00ed55f7846b
Dmytro Liubarskyi <ljubarskij@gmail.com>
2025-04-30 10:50:42 +0200

    fixed ServiceWithStreamingExample

--- other-examples/src/main/java/ServiceWithStreamingExample.java
+++ other-examples/src/main/java/ServiceWithStreamingExample.java
@@ -1,9 +1,13 @@
 import dev.langchain4j.model.chat.StreamingChatLanguageModel;
+import dev.langchain4j.model.chat.response.ChatResponse;
 import dev.langchain4j.model.openai.OpenAiStreamingChatModel;
 import dev.langchain4j.service.AiServices;
 import dev.langchain4j.service.TokenStream;
 
+import java.util.concurrent.CompletableFuture;
+
 import static dev.langchain4j.model.openai.OpenAiChatModelName.GPT_4_O_MINI;
+import static java.util.concurrent.TimeUnit.SECONDS;
 
 public class ServiceWithStreamingExample {
 
@@ -12,7 +16,7 @@ public class ServiceWithStreamingExample {
         TokenStream chat(String message);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         // Sorry, "demo" API key does not support streaming (yet). Please use your own key.
         StreamingChatLanguageModel model = OpenAiStreamingChatModel.builder()
@@ -24,9 +28,14 @@ public class ServiceWithStreamingExample {
 
         TokenStream tokenStream = assistant.chat("Tell me a joke");
 
-        tokenStream.onPartialResponse(System.out::println)
-                .onCompleteResponse(System.out::println)
-                .onError(Throwable::printStackTrace)
+        CompletableFuture<ChatResponse> futureResponse = new CompletableFuture<>();
+
+        tokenStream.onPartialResponse(System.out::print)
+                .onCompleteResponse(futureResponse::complete)
+                .onError(futureResponse::completeExceptionally)
                 .start();
+
+        ChatResponse chatResponse = futureResponse.get(30, SECONDS);
+        System.out.println("\n" + chatResponse);
     }
 }
