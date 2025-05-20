4aff691c226cae5f26935ad6f58c4ac5f61ba058
David Jiang <davidc.jiang@oracle.com>
2025-05-20 14:01:06 -0400

    Change to directory, add summary

--- oracle-example/src/main/java/OracleIngestExample.java
+++ oracle-example/src/main/java/OracleIngestExample.java
@@ -4,6 +4,8 @@ import dev.langchain4j.data.document.splitter.oracle.OracleDocumentSplitter;
 import dev.langchain4j.data.embedding.Embedding;
 import dev.langchain4j.data.segment.TextSegment;
 import dev.langchain4j.model.oracle.OracleEmbeddingModel;
+import dev.langchain4j.model.oracle.OracleSummaryLanguageModel;
+import dev.langchain4j.model.output.Response;
 import dev.langchain4j.store.embedding.EmbeddingMatch;
 import dev.langchain4j.store.embedding.EmbeddingSearchRequest;
 import dev.langchain4j.store.embedding.EmbeddingSearchResult;
@@ -13,18 +15,17 @@ import dev.langchain4j.store.embedding.oracle.EmbeddingTable;
 import dev.langchain4j.store.embedding.oracle.OracleEmbeddingStore;
 import java.io.IOException;
 import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import oracle.ucp.jdbc.PoolDataSource;
 import oracle.ucp.jdbc.PoolDataSourceFactory;
 
 /**
- * Demonstrate how to ingest documents either by using the low-level
- * LangChain4j APIs to load the documents, split the text, and get the vector
- * embeddings or the OracleEmbeddingStore to hide the manual steps of ingesting
- * into an embedding store for search/retrieval.
+ * Demonstrate how to ingest documents either by using the low-level LangChain4j
+ * APIs to load the documents, split the text, and get the vector embeddings or
+ * the OracleEmbeddingStore to hide the manual steps of ingesting into an
+ * embedding store for search/retrieval.
  *
  * This example requires the following environment variables:
  * ORACLE_JDBC_URL
@@ -33,7 +34,7 @@ import oracle.ucp.jdbc.PoolDataSourceFactory;
  * DEMO_ONNX_DIR
  * DEMO_ONNX_FILE
  * DEMO_ONNX_MODEL
- * DEMO_FILE
+ * DEMO_DIRECTORY
  */
 public class OracleIngestExample {
 
@@ -61,31 +62,38 @@ public class OracleIngestExample {
                 System.getenv("DEMO_ONNX_DIR"),
                 System.getenv("DEMO_ONNX_FILE"),
                 System.getenv("DEMO_ONNX_MODEL"));
+        
+        // set the loader, splitter, embedding, and summary model preferences
+        String loaderPref = "{\"dir\": \"" + System.getenv("DEMO_DIRECTORY") + "\"}";
+        String splitterPref = "{\"by\": \"words\", \"max\": 100}";
+        String embedderPref = "{\"provider\": \"database\", \"model\": \"" + System.getenv("DEMO_ONNX_MODEL") + "\"}";
+        String summaryPref = "{\"provider\": \"database\", \"glevel\": \"S\"}";
 
-        // Load the document that includes the information you'd like to "chat" about with the model.
-        String loaderPref = "{\"file\": \"" + System.getenv("DEMO_FILE") + "\"}";
         OracleDocumentLoader loader = new OracleDocumentLoader(conn);
+        OracleDocumentSplitter splitter = new OracleDocumentSplitter(conn, splitterPref);
+        OracleEmbeddingModel embeddingModel = new OracleEmbeddingModel(conn, embedderPref);
+        OracleSummaryLanguageModel summaryModel = new OracleSummaryLanguageModel(conn, summaryPref);
+
+        // Load the documents with the information that you would like to search on
         List<Document> docs = loader.loadDocuments(loaderPref);
 
-        // Split document into segments 100 tokens each
-        String splitterPref = "{\"by\": \"words\", \"max\": 100}";
-        OracleDocumentSplitter splitter = new OracleDocumentSplitter(conn, splitterPref);
-        List<TextSegment> segments = null;
-        for (Document doc : docs) {
-            segments = splitter.split(doc);
-            for (TextSegment segment : segments) {
-                System.out.println("segment=" + segment.text());
-            }
+        // Split document into segments
+        List<TextSegment> allSegments = new ArrayList<>();
+        for (Document doc : docs) {          
+            // For each doc, add a summary to the metadata
+            // This will get copied into each segment
+            Response<String> resp = summaryModel.generate(doc.text());
+            doc.metadata().put("summary", resp.content());
+
+            List<TextSegment> segments = splitter.split(doc);
+            allSegments.addAll(segments);
         }
 
         // Embed segments (convert them into vectors that represent the meaning) using embedding model
-        String embedderPref = "{\"provider\": \"database\", \"model\": \"" + System.getenv("DEMO_ONNX_MODEL") + "\"}";
-        OracleEmbeddingModel embeddingModel = new OracleEmbeddingModel(conn, embedderPref);
-        List<Embedding> embeddings = embeddingModel.embedAll(segments).content();
-        for (Embedding embedding : embeddings) {
-            System.out.println("embedding=" + embedding);
-        }
+        List<Embedding> embeddings = embeddingModel.embedAll(allSegments).content();
 
+        // setup the embedding store
+        
         // set column names for the output table
         String tableName = "TEST";
         String idColumn = "ID";
@@ -93,7 +101,6 @@ public class OracleIngestExample {
         String textColumn = "TEXT";
         String metadataColumn = "METADATA";
 
-        // setup the embedding store
         // build() should create a table with the configured names
         OracleEmbeddingStore embeddingStore = OracleEmbeddingStore.builder()
                 .dataSource(pds)
@@ -108,16 +115,15 @@ public class OracleIngestExample {
                 .build();
 
         // Store embeddings into embedding store for further search / retrieval
-        embeddingStore.addAll(embeddings, segments);
+        embeddingStore.addAll(embeddings, allSegments);
 
-        // Specify the question you want to ask the model
-        String question = "Who is Charlie?";
+        // Get the question
+        String question = "Who is John Doe?";
 
         // Embed the question
         Embedding questionEmbedding = embeddingModel.embed(question).content();
 
         // Find relevant embeddings in embedding store by semantic similarity
-        // You can play with parameters below to find a sweet spot for your specific use case
         EmbeddingSearchRequest embeddingSearchRequest = EmbeddingSearchRequest.builder()
                 .queryEmbedding(questionEmbedding)
                 .maxResults(3)
@@ -129,7 +135,8 @@ public class OracleIngestExample {
         System.out.println(question);
         for (EmbeddingMatch<TextSegment> match : relevantEmbeddings) {
             System.out.println("Score: " + match.score());
-            System.out.println("Text Segment: " + match.embedded().text());
+            System.out.println("Segment: " + match.embedded().text());
+            System.out.println("Metadata: " + match.embedded().metadata());
         }
     }
 
@@ -145,15 +152,6 @@ public class OracleIngestExample {
         pds.setPassword(System.getenv("ORACLE_JDBC_PASSWORD"));
         Connection conn = pds.getConnection();
 
-        // set the loader, splitter, and embedding preferences
-        String loaderPref = "{\"file\": \"" + System.getenv("DEMO_FILE") + "\"}";
-        String splitterPref = "{\"by\": \"words\", \"max\": 100}";
-        String embedderPref = "{\"provider\": \"database\", \"model\": \"" + System.getenv("DEMO_ONNX_MODEL") + "\"}";
-
-        OracleDocumentLoader loader = new OracleDocumentLoader(conn);
-        OracleDocumentSplitter splitter = new OracleDocumentSplitter(conn, splitterPref);
-        OracleEmbeddingModel embedder = new OracleEmbeddingModel(conn, embedderPref);
-
         // load the ONNX model for embedding
         OracleEmbeddingModel.loadOnnxModel(
                 conn,
@@ -161,9 +159,17 @@ public class OracleIngestExample {
                 System.getenv("DEMO_ONNX_FILE"),
                 System.getenv("DEMO_ONNX_MODEL"));
 
-        // load the document
-        List<Document> docs = loader.loadDocuments(loaderPref);
+        // set the loader, splitter, and embedding preferences
+        String loaderPref = "{\"dir\": \"" + System.getenv("DEMO_DIRECTORY") + "\"}";
+        String splitterPref = "{\"by\": \"words\", \"max\": 100}";
+        String embedderPref = "{\"provider\": \"database\", \"model\": \"" + System.getenv("DEMO_ONNX_MODEL") + "\"}";
 
+        OracleDocumentLoader loader = new OracleDocumentLoader(conn);
+        OracleDocumentSplitter splitter = new OracleDocumentSplitter(conn, splitterPref);
+        OracleEmbeddingModel embeddingModel = new OracleEmbeddingModel(conn, embedderPref);
+
+        // setup the embedding store
+        
         // set column names for the output table
         String tableName = "TEST";
         String idColumn = "ID";
@@ -171,7 +177,6 @@ public class OracleIngestExample {
         String textColumn = "TEXT";
         String metadataColumn = "METADATA";
 
-        // setup the embedding store
         // build() should create a table with the configured names
         OracleEmbeddingStore embeddingStore = OracleEmbeddingStore.builder()
                 .dataSource(pds)
@@ -185,39 +190,32 @@ public class OracleIngestExample {
                         .build())
                 .build();
 
-        // ingest the documents with the following components
+        // build an ingestor with the following components
         EmbeddingStoreIngestor ingestor = EmbeddingStoreIngestor.builder()
                 .documentSplitter(splitter)
-                .embeddingModel(embedder)
+                .embeddingModel(embeddingModel)
                 .embeddingStore(embeddingStore)
                 .build();
+
+        // load and ingest the documents
+        // this will call the splitter to split into segments,
+        // embedding model to get the embeddings, and then store the
+        // embeddings into the embedding store for further search / retrieval
+        List<Document> docs = loader.loadDocuments(loaderPref);
         ingestor.ingest(docs);
 
-        // check the chunks
-        System.out.println("chunks inserted:");
-        String queryEmbeddingStore = "select * from %s".formatted(tableName);
-        try (PreparedStatement stmt = conn.prepareStatement(queryEmbeddingStore)) {
-            try (ResultSet rs = stmt.executeQuery()) {
-                while (rs.next()) {
-                    String id = rs.getString(idColumn);
-                    String text = rs.getString(textColumn);
-
-                    String ending = text.length() > 50 ? "..." : "";
-                    System.out.println(id + "\t" + text.substring(0, 50) + ending);
-                }
-            }
-        }
-
         // get the question
-        String question = "Who is Charlie?";
+        String question = "Who is John Doe?";
 
         // get the vector representation
-        Embedding questionAsVector = embedder.embed(question).content();
+        Embedding questionAsVector = embeddingModel.embed(question).content();
 
         // perform the vector search
         EmbeddingSearchResult<TextSegment> result = embeddingStore.search(
                 EmbeddingSearchRequest.builder()
                         .queryEmbedding(questionAsVector)
+                        .maxResults(3)
+                        .minScore(0.6)
                         .build()
         );
 
@@ -226,7 +224,8 @@ public class OracleIngestExample {
         List<EmbeddingMatch<TextSegment>> results = result.matches();
         for (EmbeddingMatch<TextSegment> match : results) {
             System.out.println("Score: " + match.score());
-            System.out.println("Text Segment: " + match.embedded().text());
+            System.out.println("Segment: " + match.embedded().text());
+            System.out.println("Metadata: " + match.embedded().metadata());
         }
     }
 }
