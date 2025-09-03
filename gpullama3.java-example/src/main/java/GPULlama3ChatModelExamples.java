6bb222c361a67efe1e69d2cdeaf6c5de734e583c
mikepapadim <mikepapadim@hotmail.com>
2025-09-03 16:00:14 +0300

    Enable GPU support in GPULlama3ChatModel example and comment out metrics logging

--- gpullama3.java-example/src/main/java/GPULlama3ChatModelExamples.java
+++ gpullama3.java-example/src/main/java/GPULlama3ChatModelExamples.java
@@ -8,13 +8,17 @@ public class GPULlama3ChatModelExamples {
     public static void main(String[] args) {
 
         Path modelPath = Paths.get("beehive-llama-3.2-1b-instruct-fp16.gguf");
-        GPULlama3ChatModel model = GPULlama3ChatModel.builder().modelPath(modelPath).build();
+        GPULlama3ChatModel model = GPULlama3ChatModel
+                                    .builder()
+                                    .modelPath(modelPath)
+                                    .onGPU(true)
+                                    .build();
 
         String prompt = "What is the capital of France?";
         String res = model.chat(prompt);
         System.out.println(res);
 
         //Optionally print metrics
-        model.printLastMetrics();
+//        model.printLastMetrics();
     }
 }
