92f159208a5c6a18ffdf1d1ca53cff341b248102
mikepapadim <mikepapadim@hotmail.com>
2025-09-12 14:18:33 +0300

    Update GPULlama3 example: integrate `exec-maven-plugin`, upgrade dependencies, and enhance usage with `ChatRequest`/`ChatResponse`

--- gpullama3.java-example/src/main/java/GPULlama3ChatModelExamples.java
+++ gpullama3.java-example/src/main/java/GPULlama3ChatModelExamples.java
@@ -1,7 +1,12 @@
+import dev.langchain4j.data.message.UserMessage;
+import dev.langchain4j.model.chat.request.ChatRequest;
+import dev.langchain4j.model.chat.response.ChatResponse;
+import dev.langchain4j.model.chat.response.StreamingChatResponseHandler;
 import dev.langchain4j.model.gpullama3.GPULlama3ChatModel;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.logging.Handler;
 
 public class GPULlama3ChatModelExamples {
 
@@ -9,16 +14,23 @@ public class GPULlama3ChatModelExamples {
 
         // Change this path to the path of your model file.
         // Supports Mistral, Llama3, Phi-3, Qwen2.5 and Qwen3 in gguf format.
+
+        ChatRequest request = ChatRequest.builder()
+                .messages(UserMessage.from("What is the capital of France?"))
+                .build();
+
         Path modelPath = Paths.get("beehive-llama-3.2-1b-instruct-fp16.gguf");
+
         GPULlama3ChatModel model = GPULlama3ChatModel
                                     .builder()
                                     .modelPath(modelPath)
-                                    .onGPU(true)
+                                    .onGPU(Boolean.TRUE)
                                     .build();
 
-        String prompt = "What is the capital of France?";
-        String res = model.chat(prompt);
-        System.out.println(res);
+
+        ChatResponse response = model.chat(request);
+//        String res = model.chat(request);
+        System.out.println(response.aiMessage().text());
 
         //Optionally print metrics
 //        model.printLastMetrics();
